7852ac24759c05ff471f26245693279ab00e1fec
travisjungroth <jungroth@gmail.com>
2020-03-21 12:52:10 -0600

    Fix setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 read -rp 'Database Name: ' db_name
-read -rp 'Password: ' password
-scripts/createdb.sh db_name password
+password=$(openssl rand -hex 32)
+scripts/createdb.sh "${db_name}" "${password}"
 echo "DATABASE_URL=postgres://${db_name}_user:$password@localhost:5432/$db_name" >> .env
 key=$(openssl rand -base64 16)
 echo "DJANGO_SECRET_KEY=\"$key\"" >> .env
